d004cca8553b38c376e8671b91ac6d750f086fea
CHAMARA-PC\Asus <sarathchandra.chamara@gmail.com>
2020-09-30 01:26:33 +0530

    Completed user api with linking front end

--- routes/api/users.js
+++ routes/api/users.js
@@ -20,7 +20,7 @@ router.post('/newuser', [
         }
         const {
             firstName, lastName, mobileNo, nic, email, birthDay, gender, userName, password,
-            address: { city, country, lane, no, postalCode }, userRole: { id, userTypeCode, userTypeName}
+            address: { city, country, lane, no, postalCode }, userRole: { id, userTypeCode, userTypeName }
         } = req.body
         console.log(req.body.userRole)
         try {
@@ -94,25 +94,84 @@ router.get('/single-user/:_id', async (req, res) => {
 //update user
 router.patch('/update-user/:_id', async (req, res) => {
     try {
-        id = req.params._id;
-        const updateOps = {};
-        for (const ops in req.body) {
-            updateOps[ops.propName] = ops.value;
-        }
-        User.update({ _id: id }, { $set: req.body })
-            .exec()
-            .then(result => {
-                User.findById(id)
-                    .then(docs => {
-                        console.log("docs****", docs)
-                        res.status(200).json(docs);
-                    })
-                    .catch(err => {
-                        console.log(err)
-                        res.status(500).json({
-                            error: err
+        if (req.body.password) {
+            id = req.params._id;
+            console.log(req.body)
+            const updateOps = {};
+            const salt = await bcrypt.genSalt(10)
+            var reqData = req.body;
+            var password = req.body.password;
+            console.log(password + "password")
+            password = await bcrypt.hash(password, salt)
+            var newData = { ...reqData, password }
+            console.log(password + "hash")
+            console.log(newData);
+            for (const ops in req.body) {
+                updateOps[ops.propName] = ops.value;
+            }
+
+            User.update({ _id: id }, { $set: newData })
+                .exec()
+                .then(result => {
+                    User.findById(id)
+                        .then(docs => {
+                            console.log("docs****", docs)
+                            res.status(200).json(docs);
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            res.status(500).json({
+                                error: err
+                            });
                         });
-                    });
+                })
+        } else {
+            id = req.params._id;
+            console.log(req.body)
+            const updateOps = {};
+            // const salt = await bcrypt.genSalt(10)
+            var reqData = req.body;
+            // var password = req.body.password;
+            // console.log(password + "password")
+            // password = await bcrypt.hash(password, salt)
+            // var newData = { ...reqData, password }
+            // console.log(password + "hash")
+            // console.log(newData);
+            for (const ops in req.body) {
+                updateOps[ops.propName] = ops.value;
+            }
+
+            User.update({ _id: id }, { $set: req.body })
+                .exec()
+                .then(result => {
+                    User.findById(id)
+                        .then(docs => {
+                            console.log("docs****", docs)
+                            res.status(200).json(docs);
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            res.status(500).json({
+                                error: err
+                            });
+                        });
+                })
+        }
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server Error')
+    }
+})
+//Delete user
+router.delete('/delete-user/:_id', async (req, res) => {
+    try {
+        id = req.params._id;
+        const user = await User.remove({ _id: id })
+            .exec()
+            .then(docs => {
+                console.log(docs);
+                res.status(200).json(docs);
             })
     } catch (error) {
         console.error(error.message)
